f06d8672e7415befd53ea9b69f272963c5ecca18
sujon-chambugong <sujonchambugong@gmail.com>
2023-05-30 11:11:17 +0600

    updated pushed

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-
+import mongoose from "mongoose";
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/versity");
+    console.log("database connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+}
