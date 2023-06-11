ffdb8239c2a4061b9dd23654b01d68a12dc9e462
sujon-chambugong <sujonchambugong@gmail.com>
2023-06-11 23:17:10 +0600

    succesfully done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
 import globalErrorHandler from './app/middlwares/globalErrorHandler';
 import academicRouter from './app/modules/academicSemester/academic.route';
 import routerUser from './app/modules/user/user.route';
@@ -22,5 +23,17 @@ app.use('/api/academic', academicRouter);
 // })
 
 app.use(globalErrorHandler);
-
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'route not found',
+    errorMasseage: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
+});
 export default app;
